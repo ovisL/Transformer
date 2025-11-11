685ff1f4d44dd2d59d9da058bca15174490250a0
ovisL <didhw26@gmail.com>
2025-11-11 18:07:43 +0900

    feat: attention

--- transformer.py
+++ transformer.py
@@ -7,3 +7,18 @@ from torch.nn import CrossEntropyLoss
 from model.util import clones
 from transformers.activations import get_activation
 
+def self_attention(Q,K,V, mask=None) :
+    K_T = torch.transpose(K,-2,-1)
+    res_matmul = torch.matmul(Q,K_T)
+
+    d_K = Q.size()[-1]
+    res_scale = res_matmul/math.sqrt(d_K)
+
+    if mask is not None :
+        res_scale = res_scale.masked_fill(mask == 0, -1e20)
+    
+    res_softmax = F.softmax(res_scale, dim=-1)
+    
+    res_final_matmul = torch.matmul(res_softmax,V)
+
+    return res_final_matmul, res_softmax
